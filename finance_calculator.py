738a748ae7c65a6375795e31e24e5373383bf31c
Efemirkan <efemirkanguner@gmail.com>
2024-04-11 21:17:19 +0100

    Define a function to compute simple interest

---
+++ finance_calculator.py
@@ -0,0 +1,11 @@
+# Define simple_interest(p, r, t) function to calculate simple interest 
+def simple_interest(p, r, t):
+
+    '''         
+    "t" is the number of years that the money is being invested.
+    "P" is the amount that the user deposits.
+    "r" is the interest.
+    "A" is the total amount once the interest has been applied.
+    '''
+    # 𝐴 = 𝑃(1 + 𝑟 × 𝑡) formula 
+    return p * (1 + ((r / 100) * t))
