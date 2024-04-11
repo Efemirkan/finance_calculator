fdc701bd6bde3cd8003e27c9240ad2899b251646
Efemirkan <efemirkanguner@gmail.com>
2024-04-11 23:23:10 +0100

    Create main function ask user's choice then implement control flow

--- finance_calculator.py
+++ finance_calculator.py
@@ -1,5 +1,45 @@
 import math
 
+def main():
+
+    # Take user's input
+    user_choice = string_exception("investment", "bond")
+
+    # If user chooses "investment"
+    if user_choice == "investment":
+
+        # Take user's inputs
+        deposit = numeric_exceptions("How much would you like to deposit? ")
+        rate_investment = numeric_exceptions("Enter the interest rate: ")
+        year = numeric_exceptions("How many years are you planing to interest? ")
+        interest_type = string_exception("simple", "compound")
+
+        # If user chooses "simple"
+        if interest_type == "simple":
+
+            # Call simple_interest function to calculate simple interest then print as formatted
+            simple_interest_value = simple_interest(deposit, rate_investment, year)             
+            print(f"The amount of interest you'll earn on your investment: {simple_interest_value:,.3f}")
+
+        # If user chooses "compound"    
+        else:
+
+            # Call compound_interest function to calculate compound interest then print print as formatted        
+            compound_interest_value = compound_interest(deposit, rate_investment, year) 
+            print(f"The amount of interest you'll earn on your investment: {compound_interest_value:,.3f}")
+
+    # If user chooses "bond" 
+    else:
+
+        # Take user's inputs
+        house_value = numeric_exceptions("How much is house\'s present value? ")
+        rate_bond =  numeric_exceptions("Enter the interest rate: ")
+        months =  numeric_exceptions("How many months are you planning of taking repayment? ")
+        
+        # Call repayment function to calculate repayment then print as formatted   
+        repayment_value = repayment(house_value, rate_bond, months)   
+        print(f"You will have to repay each month: {repayment_value:,.3f}")
+
 # Define simple_interest(p, r, t) function to calculate simple interest 
 def simple_interest(p, r, t):
 
