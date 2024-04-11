344d8357060f39cfc0bb117d92f009ed547886b4
Efemirkan <efemirkanguner@gmail.com>
2024-04-11 23:18:05 +0100

    Define a function to handle errors for user's input as string

--- finance_calculator.py
+++ finance_calculator.py
@@ -33,3 +33,22 @@ def repayment(p, i, n):
     '''
     # repayment = (i * P)/(1 - (1 + i)**(-n)) formula
     return ((i / 1200) * p) / (1 - math.pow((1 + (i / 1200)), -n))
+
+# Define string_exception(option_1, option_2) funtion to handle exceptions releated to ValueError 
+def string_exception(option_1, option_2):
+
+    # While loop to ask user continuesly until they enter required input
+    while True:
+    # Take user's input and case-insensitive
+        try:
+            user_choice = input(f"Enter either '{option_1}' or '{option_2}' from the menu above to proceed: ").lower()
+            
+            # If user's input not "investment" or "bond" raise ValueError otherwise return option value
+            if user_choice not in [option_1, option_2]:
+                raise ValueError(f"PLease enter ONLY '{option_1}' or '{option_2}'")
+            
+            return user_choice
+    
+        # Print ValueError message
+        except ValueError as e:
+            print(e) 
