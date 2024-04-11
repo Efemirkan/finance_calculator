a601a05a8f6f3c5d96eadee3ce8d21d37069280b
Efemirkan <efemirkanguner@gmail.com>
2024-04-11 21:19:19 +0100

    Define a function to compute compound interest

--- finance_calculator.py
+++ finance_calculator.py
@@ -1,3 +1,5 @@
+import math
+
 # Define simple_interest(p, r, t) function to calculate simple interest 
 def simple_interest(p, r, t):
 
@@ -9,3 +11,14 @@ def simple_interest(p, r, t):
     '''
     # 𝐴 = 𝑃(1 + 𝑟 × 𝑡) formula 
     return p * (1 + ((r / 100) * t))
+
+def compound_interest(p, r, t):
+
+    '''         
+    "t" is the number of years that the money is being invested.
+    "P" is the amount that the user deposits.
+    "r" is the interest.
+    "A" is the total amount once the interest has been applied.
+    '''
+    # 𝐴 = 𝑃(1 + 𝑟)**𝑡 formula 
+    return p * math.pow((1 + (r / 100)), t)
